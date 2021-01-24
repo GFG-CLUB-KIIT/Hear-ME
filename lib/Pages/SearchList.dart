6c1363d0b40ccb655d85e1d383342cfd7f3d8612
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-01-24 19:41:45 +0530

    Search List Implemented

--- lib/Pages/SearchList.dart
+++ lib/Pages/SearchList.dart
@@ -13,53 +13,54 @@ class _SearchListState extends State<SearchList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: searchController,
-          decoration: InputDecoration(
-            hintText: "Search",
+        appBar: AppBar(
+          title: TextField(
+            controller: searchController,
+            decoration: InputDecoration(
+              hintText: "Search",
+            ),
           ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () async {
+                //searchController.text = "";
+                var songLists = await fetchSongsList(searchController.text);
+                setState(() {
+                  songList = songLists;
+                  print(songList);
+                });
+              },
+            )
+          ],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){
-              //searchController.text = "";
-              setState(() async{
-              songList = await fetchSongsList(searchController.text);  
-              print(songList);
-              });
-              
-            },
-          )
-        ],
-      ),
-      body:ListView.builder(
-        itemCount: songList.length,
-        itemBuilder: (context,index){
-        return Card(child: ListTile(
-          title: Text("Found"),
-        ));
-      })
-      // body:FutureBuilder(
-      //   future: fetchSongsList(searchController.text) ,
-      //   builder: (context, snapshot) {
-      //         if (snapshot.hasData) {
-      //           return Text("Found");
-      //         } else if (snapshot.hasError) {
-      //           return Text("{snapshot.error}");
-      //         }
-      //         return CircularProgressIndicator();
-      //   // builder:(context,snapshots){
-      //   //   if(snapshots.hasData){
-      //   //     return Container(color:Colors.red);
-      //   //   }
-      //   //   else{
-      //   //     return Container(color:Colors.green);
-      //   //   }
-      //   // },
-      //   },
-      // ),
-    );
+        body: ListView.builder(
+            itemCount: songList.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  child: ListTile(
+                title: Text(songList.toString()),
+              ));
+            })
+        // body:FutureBuilder(
+        //   future: fetchSongsList(searchController.text) ,
+        //   builder: (context, snapshot) {
+        //         if (snapshot.hasData) {
+        //           return Text("Found");
+        //         } else if (snapshot.hasError) {
+        //           return Text("{snapshot.error}");
+        //         }
+        //         return CircularProgressIndicator();
+        //   // builder:(context,snapshots){
+        //   //   if(snapshots.hasData){
+        //   //     return Container(color:Colors.red);
+        //   //   }
+        //   //   else{
+        //   //     return Container(color:Colors.green);
+        //   //   }
+        //   // },
+        //   },
+        // ),
+        );
   }
 }
