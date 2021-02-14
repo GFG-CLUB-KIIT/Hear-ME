8bb6022b81b68d3813b94aa535982441f5623f04
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-14 21:19:27 +0530

    signin and register button

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -8,7 +8,8 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: double.infinity,
+        // width: double.infinity,
+        // height: MediaQuery.of(context).size.height*0.3,
         decoration: BoxDecoration(
           gradient: LinearGradient(begin: Alignment.topCenter, colors: [
             Color.fromRGBO(9, 18, 39, 1),
@@ -21,7 +22,8 @@ class LoginPage extends StatelessWidget {
           children: <Widget>[
             SizedBox(height: 80,),
             Header(),
-            Expanded(child: Container(
+            Container(
+              // height: MediaQuery.of(context).size.height*0.7,
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -30,7 +32,74 @@ class LoginPage extends StatelessWidget {
                   )
               ),
               child: InputWrapper(),
-            ))
+            ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               InkWell(
+                        child: Container(
+                            width: MediaQuery.of(context).size
+                                .width/2.5,
+                            height: MediaQuery.of(context).size.height/18,
+                            margin: EdgeInsets.only(top: 25),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color:Colors.white
+                            ),
+                            child: Center(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Text('Sign in with',
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 30.0,
+                                      width: 30.0,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                            AssetImage('Assets/Images/google.png'),
+                                            fit: BoxFit.cover),
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                            )
+                        ),
+                        onTap: (){}
+               ),
+               InkWell(
+                        child: Container(
+                            width: MediaQuery.of(context).size
+                                .width/2.5,
+                            height: MediaQuery.of(context).size.height/18,
+                            margin: EdgeInsets.only(top: 25),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color:Colors.white
+                            ),
+                            child: Center(
+                                child:
+                                Text('Register',
+                                      style: TextStyle(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black
+                                      ),
+                                    ),
+                            )
+                        ),
+                        
+                        onTap: (){}
+               ),
+             ],
+           ),
           ],
         ),
       ),
