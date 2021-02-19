d86d1959492eb5823d93541b8bf46397c52fbada
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-02-19 20:20:26 +0530

    Created Playlist Page

---
+++ lib/Pages/Playlist.dart
@@ -0,0 +1,108 @@
+import 'package:HearMe/API/saavn.dart';
+import 'package:HearMe/Pages/searchList.dart';
+import 'package:HearMe/Widgets/playControl.dart';
+import 'package:flutter/material.dart';
+
+import '../Widgets/label.dart';
+import '../Widgets/musicVList.dart';
+import './Player.dart';
+
+class Playlist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(9, 18, 39, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: IconButton(
+              icon: Icon(Icons.music_note),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SearchList()));
+              },
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // User Defined Widget in ../Widgets/label.dart
+                  /*
+                    lable function provides a way to reuse the code. 
+                  */
+                  label("Playlist"),
+                  FutureBuilder(
+                    future: topSongs(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return musicVList(context, snapshot.data);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: BottomSheet(
+          backgroundColor: Colors.transparent,
+          onClosing: () {},
+          builder: (BuildContext context) {
+            return Container(
+              color: Color.fromRGBO(9, 18, 39, 1),
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "Assets/Images/monsters-go-bump.jpg",
+                          fit: BoxFit.cover,
+                          width: 60,
+                          height: 60,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Monsters Go Bump"),
+                            Text("ERIKA RECINOS",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 10,
+                                )),
+                          ],
+                        )
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Player()));
+                    },
+                  ),
+                  playControl(MainAxisAlignment.spaceBetween),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
