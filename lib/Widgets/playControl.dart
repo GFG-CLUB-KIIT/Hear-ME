7364bca68066597675ee61daf682f6d3d8b69e02
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-02-20 22:06:27 +0530

    Player Control Created

--- lib/Widgets/playControl.dart
+++ lib/Widgets/playControl.dart
@@ -1,21 +1,62 @@
 import 'package:flutter/material.dart';
 
-Row playControl (MainAxisAlignment alignment){
+Row playControl(MainAxisAlignment alignment) {
+  LinearGradient gradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [Colors.indigo.withOpacity(0.6), Colors.black]);
+
+  // BoxShadow shadow = BoxShadow(
+  //   color: Colors.grey,
+  //   spreadRadius: 2,
+  //   blurRadius: 2,
+  //   offset: Offset(2, 4),
+  // );
+
+  BoxDecoration decoration = BoxDecoration(
+    gradient: gradient,
+    borderRadius: BorderRadius.all(
+      Radius.circular(100),
+    ),
+    // boxShadow: [shadow],
+  );
+
   return Row(
     mainAxisAlignment: alignment,
-  children: [
-    Icon(
-      Icons.arrow_left,
-      size: 40,
-    ),
-    Icon(
-      Icons.pause,
-      size: 40,
-    ),
-    Icon(
-      Icons.arrow_right,
-      size: 40,
-    ),
-  ],
-); 
-} 
+    children: [
+      Container(
+        child: IconButton(
+          icon: Icon(Icons.arrow_left),
+          iconSize: 40,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        decoration: decoration,
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      Container(
+        child: IconButton(
+          icon: Icon(Icons.pause),
+          iconSize: 50,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        decoration: decoration,
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      Container(
+        child: IconButton(
+          icon: Icon(Icons.arrow_right),
+          iconSize: 40,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        decoration: decoration,
+      ),
+    ],
+  );
+}
