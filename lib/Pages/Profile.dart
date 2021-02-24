a43b9669e78a4e483d311f25be99af771bb3d70a
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-24 13:06:27 +0530

    fields added

--- lib/Pages/Profile.dart
+++ lib/Pages/Profile.dart
@@ -58,7 +58,11 @@ class _ProfilePage extends State<ProfilePage> {
             ),
           ]),
       body: Container(
-          padding: EdgeInsets.only(left: 30, top: 25, right: 16),
+        padding: EdgeInsets.only(left: 30, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
           child: ListView(
             children: [
               Text(
@@ -78,18 +82,77 @@ class _ProfilePage extends State<ProfilePage> {
                       width: 130,
                       height: 130,
                       decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/62473188?s=460&u=467228de100d967026257b0a126d991bc5c7506d&v=4"),
-                          )),
+                        border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.white.withOpacity(0.1),
+                            offset: Offset(0, 5),
+                          )
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/62473188?s=460&u=467228de100d967026257b0a126d991bc5c7506d&v=4"),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: Icon(Icons.edit, color: Colors.white),
+                      ),
                     )
                   ],
                 ),
-              )
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              buildTextField("Full Name", "Ashish Mishra"),
+              buildTextField("Email", "test@gmail.com"),
+              buildTextField("Password", "admin@123"),
+              buildTextField("Location", "xyz , India "),
             ],
-          )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(String labelText, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(bottom: 5),
+          labelText: labelText,
+          labelStyle: TextStyle(
+            color: Colors.white,
+            // fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: 14,
+            // fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
